765484d1d8896f5f7aae8f89e8a631eec8106d32
ViniciusOJ <contato.2002vinicius@gmail.com>
2023-02-08 12:27:48 -0300

    login function, signOut function, generate cookie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from "react";
+import { useContext, FormEvent, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import * as Styled from "../styles/Login/login";
@@ -13,12 +13,18 @@ import { AuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
   const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
   async function handleLogin(ev: FormEvent) {
     ev.preventDefault();
     let data = {
-      email: "vinicius@teste.com",
-      password: "123"
-    }
+      email,
+      password,
+    };
     await signIn(data);
   }
 
@@ -31,8 +37,18 @@ export default function Home() {
         <Image src={Logo} alt="Logo Viny Lanches" />
         <Styled.Login>
           <Styled.Form onSubmit={handleLogin}>
-            <Input placeholder="Digite seu email" type="text" />
-            <Input placeholder="Digite seu senha" type="password" />
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+            <Input
+              placeholder="Digite seu senha"
+              type="password"
+              value={password}
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
             <Button type="submit" loading={false}>
               Acessar
             </Button>
